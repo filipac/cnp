b40b4d2d7c11531dc023aceb04f31b1d8ac50056
Filip Pacurar Geek <hello@filipac.net>
2015-03-02 18:15:01 +0200

    prima versiune test

--- tests/CnpTest.php
+++ tests/CnpTest.php
@@ -1,13 +1,22 @@
 <?php
 use Filipac\Cnp\Cnp;
- 
-class CnpTest extends PHPUnit_Framework_TestCase {
- 
-  /** @test */
-  public function testNachHasCheese()
-  {
-    $nacho = new Cnp;
-    $this->assertTrue($nacho->hasCheese());
-  }
- 
+
+class CnpTest extends PHPUnit_Framework_TestCase
+{
+
+    /** @test */
+    public function testCnpValid()
+    {
+        $valid = Cnp::valid('1930101021162');
+        $this->assertTrue($valid);
+    }
+
+
+    /** @test */
+    public function testCnpInvalid()
+    {
+        $valid = Cnp::valid('1930101021161');
+        $this->assertFalse($valid);
+    }
+
 }
