5af3fe77929b5d561e7fae7d15d532e08001d373
Razvan Grigore <razvanphp@yahoo.com>
2020-01-09 16:25:21 +0100

    code linting as per phpcs rules

--- src/Laravel/CnpValidatorProvider.php
+++ src/Laravel/CnpValidatorProvider.php
@@ -1,37 +1,36 @@
 <?php
+
 namespace Filipac\Cnp\Laravel;
+
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 use Filipac\Cnp\Cnp;
 
-class CnpValidatorProvider extends ServiceProvider {
-
-  /**
-   * Bootstrap the application services.
-   *
-   * @return void
-   */
-  public function boot()
-  {
-    Validator::extend('cnp', function($attribute, $value, $parameters)
+class CnpValidatorProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
     {
-      return Cnp::valid($value);
-    });
-    Validator::replacer('cnp', function($message, $attribute, $rule, $parameters)
+        Validator::extend('cnp', function ($attribute, $value, $parameters) {
+            return Cnp::valid($value);
+        });
+        Validator::replacer('cnp', function ($message, $attribute, $rule, $parameters) {
+            $message = "Cnp-ul introdus nu este corect!";
+            return $message;
+        });
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
     {
-      $message = "Cnp-ul introdus nu este corect!";
-      return $message;
-    });
-  }
-
-  /**
-   * Register the application services.
-   *
-   * @return void
-   */
-  public function register()
-  {
-    //
-  }
-
+      //
+    }
 }
